6037816c66ee2c5372eb80e6dfe72b06b17349ba
ruanqiaohua <ruanqiaohua@hotmail.com>
2017-04-27 11:47:40 +0800

    资源

---
+++ ZQAlertViewController/ZQPickViewController/ZQPickViewController.h
@@ -0,0 +1,35 @@
+//
+//  ZQPickViewController.h
+//  Demo
+//
+//  Created by 阮巧华 on 2017/3/23.
+//  Copyright © 2017年 阮巧华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQPickViewController : UIViewController
+
+typedef void(^SelectedCallBack)(ZQPickViewController *alertController,NSInteger selectedIndex);
+
++ (instancetype)alertControllerWithTitle:(NSString *)title message:(NSArray<NSString *> *)messages images:(NSArray<NSString *> *)images selectedCallBack:(SelectedCallBack)callBack;
+
+@property (nonatomic, strong) UIView *contentView;
+
+@property (nonatomic, assign) CGFloat spacing;
+
+@property (nonatomic, assign) CGFloat lineSpacing;
+
+@property (nonatomic, assign) BOOL arrowHidden;
+
+@property (nonatomic, strong) NSMutableArray <UIButton *> *buttons;
+
+- (void)setTextColor:(UIColor *)color;
+
+- (void)setStartPoint:(CGPoint)point;
+
+- (void)show;
+
+- (void)hidden;
+
+@end
